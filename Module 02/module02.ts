4f91b1629545d41b4ca22b86dd0c6887adaa110e
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2022-06-12 10:16:45 +0200

    exercise01 complete

--- Module 02/module02.ts
+++ Module 02/module02.ts
@@ -5,22 +5,34 @@
     TODO: Modify the code to add types to the variable declarations. 
     The resulting JavaScript should look the same as the original example when you're done. */
 
-let firstName;
-let lastName;
-let fullName;
-let age;
-let ukCitizen;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let age: number;
+let ukCitizen: boolean;
 
 firstName = 'Rebecca';
 lastName = 'Smith';
 age = 42;
 ukCitizen = false;
-fullName = firstname + " " + lastname;
+fullName = firstName + ' ' + lastName;
 
 if (ukCitizen) {
-    console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
+    console.log(
+        'My name is ' +
+            fullName +
+            ", I'm " +
+            age +
+            ", and I'm a citizen of the United Kingdom."
+    );
 } else {
-    console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
+    console.log(
+        'My name is ' +
+            fullName +
+            ", I'm " +
+            age +
+            ", and I'm not a citizen of the United Kingdom."
+    );
 }
 
 /* EXERCISE 2
@@ -45,39 +57,36 @@ console.log(a);
    Season.Fall, instead of the literal string "Fall". */
 
 function whichMonths(season) {
-
     let monthsInSeason: string;
 
     switch (season) {
-        case "Fall":
-            monthsInSeason = "September to November";
+        case 'Fall':
+            monthsInSeason = 'September to November';
             break;
-        case "Winter":
-            monthsInSeason = "December to February";
+        case 'Winter':
+            monthsInSeason = 'December to February';
             break;
-        case "Spring":
-            monthsInSeason = "March to May";
+        case 'Spring':
+            monthsInSeason = 'March to May';
             break;
-        case "Summer":
-            monthsInSeason = "June to August";
+        case 'Summer':
+            monthsInSeason = 'June to August';
     }
 
     return monthsInSeason;
 }
 
-console.log(whichMonths("Fall"));
+console.log(whichMonths('Fall'));
 
 /* EXERCISE 4
    TODO: Declare the array as the type to match the type of the items in the array. */
 
-   let randomNumbers;
-   let nextNumber;
-   
-   for (let i = 0; i < 10; i++) {
-       nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-       randomNumbers.push(nextNumber);
-   }
-   
-   console.log(randomNumbers);
+let randomNumbers;
+let nextNumber;
 
-   
+for (let i = 0; i < 10; i++) {
+    nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    randomNumbers.push(nextNumber);
+}
+
+console.log(randomNumbers);
